9ca8db9b89c4b38d60bc5a5fb211057655cd330b
olfj <ole.fjeldsaa@live.com>
2025-01-13 15:20:56 +0100

    Added displacement of walker

--- src/run.c
+++ src/run.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 
 void task1();
+gsl_rng* get_rand(void);
 
 int
 run(
@@ -24,6 +25,7 @@ void task1(){
     double* walkers = linspace(-5, 5, 200, false);
     double E_t = 0.5;
     double delta_tau = 0.02;
+    gsl_rng* r = get_rand();
 
 
 
@@ -31,4 +33,19 @@ void task1(){
 
 }
 
+float displace_x(float x, float delta_tau, gsl_rng* r)
+{
+    float G = gsl_ran_gaussian(r, 1);
+    return x + sqrt(delta_tau)*G;
+}
 
+gsl_rng* get_rand(void){
+    const gsl_rng_type* T;
+    gsl_rng* r;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc(T);
+    time_t seed = time(NULL);
+    gsl_rng_set(r, seed);
+    return r;
+}
