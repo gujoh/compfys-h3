ad552a9fb2705101bc56057ec1b241cee907d769
gujoh <gustafjohanson@live.com>
2025-01-15 17:02:03 +0100

    task3 start

--- src/run.c
+++ src/run.c
@@ -26,11 +26,11 @@ double displace_x(double x, double delta_tau, gsl_rng* r);
 double weight_1d(double x, double E_t, double dt);
 double weight_6d(double* walker, double E_t, double dt);
 double update_E_t(double E_t, double gamma, int n, int n0);
-double wave(double* r1, double* r2, double alpha);
 result_dmc diffusion_monte_carlo_1d(double* walkers, int n0, double E_t, double gamma, double dt, int n_iter, int n_eq);
-result_dmc diffusion_monte_carlo_6d(double** walkers, int n0, double E_t, double gamma, double dt, int n_iter, int n_eq);
+result_dmc diffusion_monte_carlo_6d(double** walkers, int n0, double E_t, double gamma, double dt, int n_iter, int n_eq, int add_drift);
 double** init_walkers_6d(int n);
 double** polar_to_cart(double** polar, int n);
+void drift1(double* walker, double alpha, double dt);
 
 int
 run(
@@ -39,7 +39,8 @@ run(
    )
 {
     //task1();
-    task2();
+    //task2();
+    task3();
     return 0;
 }
 
@@ -65,7 +66,7 @@ void task2(void)
     double gamma = 0.5;
     int n_iter = 500000;
     int n_eq = 1500;
-    result_dmc result = diffusion_monte_carlo_6d(walkers_cartesian, n, E_t, gamma, delta_tau, n_iter, n_eq);
+    result_dmc result = diffusion_monte_carlo_6d(walkers_cartesian, n, E_t, gamma, delta_tau, n_iter, n_eq, 0);
 }
 
 void task3(void)
@@ -76,9 +77,9 @@ void task3(void)
     double E_t = - 3;
     double delta_tau = 0.01;
     double gamma = 0.5;
-    int n_iter = 500000;
+    int n_iter = 20000;
     int n_eq = 1500;
-    result_dmc result = diffusion_monte_carlo_6d(walkers_cartesian, n, E_t, gamma, delta_tau, n_iter, n_eq);
+    result_dmc result = diffusion_monte_carlo_6d(walkers_cartesian, n, E_t, gamma, delta_tau, n_iter, n_eq, 1);
 }
 
 result_dmc diffusion_monte_carlo_1d(double* walkers, int n0, double E_t, double gamma, double dt, int n_iter, int n_eq)
@@ -151,13 +152,13 @@ result_dmc diffusion_monte_carlo_1d(double* walkers, int n0, double E_t, double 
     return result;
 }
 
-result_dmc diffusion_monte_carlo_6d(double** walkers, int n0, double E_t, double gamma, double dt, int n_iter, int n_eq)
+result_dmc diffusion_monte_carlo_6d(double** walkers, int n0, double E_t, double gamma, double dt, int n_iter, int n_eq, int add_drift)
 {
     gsl_rng* r = get_rand();
     int n = n0;
     double E_t_sum = 0;
-    FILE* file = fopen("data/task2.csv", "w+");
-    FILE* positions = fopen("data/positions_task2.csv", "w+");
+    FILE* file = fopen("data/task3.csv", "w+");
+    FILE* positions = fopen("data/positions_task3.csv", "w+");
     int* walker_multiplier;
     double** new_walkers;
     int dim = 6;
@@ -180,9 +181,12 @@ result_dmc diffusion_monte_carlo_6d(double** walkers, int n0, double E_t, double
             {
                 walkers[j][k] = displace_x(walkers[j][k], dt, r);
             }
+            if (add_drift == 1)
+            {
+                drift1(walkers[j], 0.15, dt);
+            }
             double w = weight_6d(walkers[j], E_t, dt);
             int m = (int) (w  + gsl_rng_uniform(r));
-            //printf("%lf\n", w / dim);
             walker_multiplier[j] = m;
             multiplier_sum += m;
         }
@@ -278,13 +282,28 @@ double weight_6d(double* walker, double E_t, double dt)
     return exp(- (v - E_t) * dt);
 } 
 
-double wave(double* r1, double* r2, double alpha)
-{   
-    double r1_len = vector_norm(r1, 3);
-    double r2_len = vector_norm(r2, 3);
+void drift1(double* walker, double alpha, double dt)
+{
+    double r1[] = {walker[0], walker[1], walker[2]};
+    double r2[] = {walker[3], walker[4], walker[5]};
+    double r1_norm[3]; 
+    double r2_norm[3];
+    memcpy(r1_norm, r1, sizeof(r1_norm)); 
+    memcpy(r2_norm, r2, sizeof(r2_norm)); 
+    normalize_vector(r1_norm, 3);
+    normalize_vector(r2_norm, 3);
+    double r12_norm[3];
+    elementwise_subtraction(r12_norm, r1, r2, 3);
+    normalize_vector(r12_norm, 3);
     double r12_len = distance_between_vectors(r1, r2, 3);
-    return exp(- 2 * r1_len) * exp(- 2 * r2_len) * 
-        exp(r12_len / (2 + 2 * alpha * r12_len));
+    for (int i = 0; i < 3; i++)
+    {
+        walker[i] += (- 2 * r1_norm[i] - r12_norm[i] / (2 * pow(1 + alpha * r12_len, 2))) * dt;
+    }
+    for (int i = 3; i < 6; i++)
+    {
+        walker[i] += (- 2 * r2_norm[i] + r12_norm[i] / (2 * pow(1 + alpha * r12_len, 2))) * dt;
+    }
 }
 
 // double E_l(double* walker, double E_t, double alpha, double dt)
